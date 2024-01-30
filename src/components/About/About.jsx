a00ae39e73d9086e4abd604319c3955b4012d5e5
Matthieu-Simon <simon.matthieu89@gmail.com>
2024-01-30 17:43:23 +0100

    Correction text section about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,8 +10,9 @@ const About = () => {
                 <div className="description-about-content">
                     <h1 className="title-about">Matthieu Simon</h1>
                     <h2 className="subtitle">Développeur Front-end.</h2>
-                    <p className="text-about">Bonjour, je suis Matthieu Simon, passioné par le développement web et particulièrement le développement front-end avec une spécialité en ReactJS. Aprés avoir découvert la programmation, j'ai décidé de m'y consacrer pleinement. J'ai suivi une formation d'intégrateur web avec OpenClassRooms ou j'ai pu acquérir des bases en développement web en apprenant le HTML, CSS, le langage JavaScript ainsi que sa bibliothèque ReactJS en réalisant différents projets.</p>
-                    <p className="text-about">En plus de ma formation, je suis maintien une veille concernant les dernières tendances et technologies du développement web. Je continue mon apprentissage autour de l'ecosystème JavaScript.</p>
+                    <p className="text-about">Bonjour, je suis Matthieu Simon, passioné par le développement web et particulièrement le développement front-end avec une spécialité en ReactJS.
+                    Aprés avoir découvert la programmation, j'ai décidé de m'y consacrer pleinement. J'ai suivi une formation d'intégrateur web avec OpenClassRooms ou j'ai pu acquérir des bases en développement web en apprenant le HTML, CSS, le langage JavaScript ainsi que sa bibliothèque ReactJS en réalisant différents projets.</p>
+                    <p className="text-about">En plus de ma formation, je maintiens une veille concernant les dernières tendances et technologies du développement web. Je continue mon apprentissage autour de l'ecosystème JavaScript.</p>
                     <p className="text-about">Je suis actuellement ouvert à de nouvelles opportunités où je pourrais contribuer, apprendre et évoluer en tant que développeur web et me perfectionner dans la création d'interfaces utilisateur dynamiques.</p>
                 </div>
                 <div className="link-about">
